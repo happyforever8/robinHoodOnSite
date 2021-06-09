764d73051b731ab9b1249f85aa908fe47eaefd73
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-06-09 11:40:20 -0400

    Update Sliding_Window_Median.java

--- Sliding_Window_Median.java
+++ Sliding_Window_Median.java
@@ -64,3 +64,69 @@ class Solution {
         return result;
     }
 }
+
+
+295. Find Median from Data Stream
+
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+
+class MedianFinder {
+    PriorityQueue<Integer> min;
+    PriorityQueue<Integer> max;
+    boolean odd;
+    
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        min = new PriorityQueue<>();
+        max = new PriorityQueue<>((a, b) -> b - a);
+        odd = true;   
+    }
+    
+    public void addNum(int num) {
+        if (odd){
+            min.offer(num);
+            max.offer(min.poll());
+        } else {
+            max.offer(num);
+            min.offer(max.poll());
+        }
+        odd = !odd;
+    }
+    
+    public double findMedian() {
+        int size1 = min.size();
+        int size2 = max.size();
+        
+        if ((size1 + size2) % 2 == 0){
+            return ((double)min.peek() + (double)max.peek())/ 2;
+        } else {
+            return (double)max.peek();
+        }
+    }
+}
